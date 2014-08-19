31327ba93840871b71ea219a620cf6b053efde40
Jay Balunan <iamjeajae@gmail.com>
2014-08-19 09:47:12 +0800

    Splash Screen Commit

--- CityGatesChristianCenter/src/com/example/citygates/Splash.java
+++ CityGatesChristianCenter/src/com/example/citygates/Splash.java
@@ -1,34 +1,24 @@
 package com.example.citygates;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Intent;
+import android.os.Handler;
 
-public class Splash extends ActionBarActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
-	}
+public class Splash extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.splash, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+                public void run() {
+                        startActivity(new Intent(Splash.this, Home.class));
+                        finish();
+                }
+        }, secondsDelayed * 1000);
+    }
 }
